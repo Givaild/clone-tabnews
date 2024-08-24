c9a7e0ddbd791921b75b1f77549c9d66c94292e4
Givaildo de Andrade <65353339+Givaild@users.noreply.github.com>
2024-08-24 22:24:53 +0000

    fix: insert more emojis

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home() {
     return <h1>
-    💥 💥 💞Eu Amo você, 💞 se você me ama, da uma risadinha 😁😁😁  💥 💥 
+💥  💥 💥 💞Eu Amo você, 💞 se você me ama, da uma risadinha 😁😁😁  💋💋💋💥 💥 💥 
     </h1>
 }
 
